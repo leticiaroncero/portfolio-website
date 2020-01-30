291a09873e5ce2bed0ee8ea0b45f985f13ff11b7
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2020-01-30 15:02:06 -0800

    add header and minor styling

--- controllers/routes.js
+++ controllers/routes.js
@@ -4,7 +4,7 @@ var db = require("../models");
 var router = express.Router();
 
 router.get("/", function (req, res) {
-    res.sendfile("index.html", { 'root': "./views" });
+    res.sendFile("index.html", { 'root': "./views" });
 });
 
 module.exports = router;
