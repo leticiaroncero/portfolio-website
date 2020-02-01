d0bc6019ddcbefd755f4d3a856256ddc7fc2d4d6
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2020-01-31 19:51:20 -0800

    update header image and minor changes

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,12 +1,6 @@
 var typed = new Typed('.typed', {
-    strings: ["a technical wizard.", "a full stack developer."],
+    strings: ["a full stack developer.", "Leticia Roncero."],
     typeSpeed: 30,
     loop: true,
     loopCount: 3
 });
-
-// $(document).ready(function () {
-//     $('.contactForm').submit(function (event) {
-//         $('submit').attr('value', 'Message Sent!');
-//     })
-// });
